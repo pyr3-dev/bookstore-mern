21dd36dc652b1152c2c6b3012901f742e516a8fd
pyr3-dev <jjustoe04@gmail.com>
2024-05-08 17:11:21 +0800

    config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,5 @@
 export const PORT = 5555;
-export const MONGODBURL = `mongodb+srv://jjustoe04:5hnFcxJMTGrWWBys@cluster0.af4cyx0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-export const DB_USERNAME = 'jjustoe04';
-export const DB_PASSWORD = '5hnFcxJMTGrWWBys';
+// Provide your own MongoDB URL, username and password
+export const MONGODBURL = `mongodb+srv://<username>:<password>@cluster0.af4cyx0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+export const DB_USERNAME = '<redacted>';
+export const DB_PASSWORD = '<redacted>';
